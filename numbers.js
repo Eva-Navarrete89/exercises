333b3eb8b5c528524d448db4e2c50c2c4539cf23
Eva Navarrete <EvaNavarrete@gmail.com>
2020-07-12 20:46:15 -0600

    day 1 exercise

---
+++ numbers.js
@@ -0,0 +1,20 @@
+/*
+In the exercises below, write your own code where indicated
+to achieve the desired result. You should be able to run this
+file from your terminal with the command `node day_3/exercises/comparisons.js`
+*/
+
+// example: log the result of the sum of 2 and 2 to the console:
+console.log(2 + 2);
+
+// log the result of 7 subtracted from 83 console:
+console.log(83 - 7/*YOUR CODE HERE*/);
+
+console.log(53 * 6);// log the result of 6 multiplied by 53 to the console:
+// YOUR CODE HERE
+
+// log the result of 20 divided by 4 to console:
+console.log(20 / 4);// YOUR CODE HERE
+
+// log the result of the modulo of 10 into 54:
+// YOUR CODE HERE
